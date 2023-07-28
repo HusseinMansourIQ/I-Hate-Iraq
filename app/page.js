37dae4ed5b2b47019c7c4130b1aa1fb757b8a7a3
HusseinMansourIQ <70377627+HusseinMansourIQ@users.noreply.github.com>
2023-07-28 18:08:51 +0300

    done infinite scrolling and pagination (server side)

--- app/page.js
+++ app/page.js
@@ -1,13 +1,54 @@
+import clsx from 'clsx'
 import Image from 'next/image'
+import Link from 'next/dist/client/link';
 import { prisma } from '@/lib/prisma';
-export default async function Home() {
-const posts = await prisma.Post.findMany();
+import Trigger from './trigger';
 const moment = require('moment');
+
+
+export default async function Home({searchParams}) {
+ const page =  typeof searchParams.page === 'string' ? Number(searchParams.page) : 1
+ const limit =typeof searchParams.limit === 'string' ? Number(searchParams.limit) : 5
+
+ console.log(page + "dddddddddddddddddd")
+const posts = await prisma.Post.findMany({
+  take: limit,
+  skip: (page-1)*limit ,
+});
 
   //server
   return (
     
     <main className=" space-y-4 flex-col items-center justify-between p-24">
+      
+<div className='flex space-x-6'>
+            <Link
+              href={{
+                pathname: '/',
+                query: {
+                  page: page > 1 ? page - 1 : 1
+                }
+              }}
+              className={clsx(
+                'rounded border bg-gray-100 px-3 py-1 text-sm text-gray-800',
+                page <= 1 && 'pointer-events-none opacity-50'
+              )}
+            >
+              Previous
+            </Link>
+            <Link
+              href={{
+                pathname: '/',
+                query: {
+                  page: page + 1
+                }
+              }}
+              className='rounded border bg-gray-100 px-3 py-1 text-sm text-gray-800'
+            >
+              Next
+            </Link>
+          </div>
+
     {posts.map((posts) => {
       return(
         <a href="#" class="block max-w p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -18,7 +59,9 @@ const moment = require('moment');
 
 </a>
       )
+      
     })}
+    <Trigger limit={limit}></Trigger>
 
 
     </main>
